0dff534903f705ace84d11c70979f4bfa9b261c4
Joanne Seiler <joanne@posteo.de>
2022-03-02 09:44:30 +0100

    conditional rend ering for login/register

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,30 @@
+import { useContext } from "react";
 import { NavLink } from "react-router-dom";
 import  { AuthContext } from "../context/auth.context"
 
 export default function Header() {
+
+  const {isLoggedIn, user} = useContext(AuthContext);
+
   return (
     <div className="Header">
+
         <NavLink to="/">Home</NavLink> |
-        <NavLink to="/projects">Projects</NavLink> |
-        <NavLink to="/projects/create">New Project</NavLink> |
+
+        { isLoggedIn && 
+          <>
+            <NavLink to="/projects">Projects</NavLink> |
+            <NavLink to="/projects/create">New Project</NavLink> |
+          </>
+        }
+
+        { !isLoggedIn && 
+          <>
+            <NavLink to="/signup">Register</NavLink> |
+            <NavLink to="/login">Login</NavLink> |
+          </>
+        }
+
     </div>
   )
 }
